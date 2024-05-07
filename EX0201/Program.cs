7643b3fbef447866a23a908f282d6aaf0323dcc5
miakojason <miakojason@gmail.com>
2024-05-07 17:51:07 +0800

    consoloe 輸入名字回應

---
+++ EX0201/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace EX0201
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("hello world!");
+            WriteLine("Visual C#:");
+        }
+    }
+}
---
+++ Ex0204/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ex0204
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("請輸入你的名字:");
+            string name = ReadLine();
+            WriteLine($"Good Day! {name}");
+        }
+    }
+}
