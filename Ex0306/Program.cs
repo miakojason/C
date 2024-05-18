73c1dba161949b56e115e15c147e1b80abbba331
miakojason <miakojason@gmail.com>
2024-05-18 15:06:30 +0800

    3-21

---
+++ Ex0306/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ex0306
+{
+    enum City :short //各城市郵遞區號
+    {
+        Taipei=100,
+        Sinjhu=300,
+        Taijhong=400,
+        Taninan=700,
+        Kaohsiung=800
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            City zone1, zone2;
+            short pt1, pt2;
+            zone1 = City.Kaohsiung;
+            zone2 = City.Taninan;
+            pt1 = (short)City.Sinjhu;
+            pt2 = (short)City.Taijhong;
+            WriteLine($"城市:{zone1},{zone2}");
+            WriteLine($"新竹、台中的郵遞區號:{pt1},{pt2}");
+
+        }
+    }
+}
