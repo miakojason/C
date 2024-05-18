6be873f9eeaed453a18d772d439b227044a0c4cb
miakojason <miakojason@gmail.com>
2024-05-18 16:08:16 +0800

    ex0412 for找基數累加

---
+++ Ex0412/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+//題目找出count,只找基數到20, sum累加 60停。
+int count, sum = 0;
+for (count = 0; count <= 20; count++)
+{
+    if (count % 2 == 0)
+    {
+        continue;
+    }
+    sum += count;
+    if (sum >= 60)
+    {
+        break;
+    }
+        WriteLine($"Count ={count},Sum={sum}");
+}
+ReadKey();
