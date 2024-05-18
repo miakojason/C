5a09eedc33419df6f881bc06f2149a481dadf455
miakojason <miakojason@gmail.com>
2024-05-18 15:25:17 +0800

    建立stuct結構定義個別變數資料型別

---
+++ Ex0307/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ex0307
+{
+    struct Compter
+    {//價格price, 序號serial,製造日期madeDate
+        public int price;
+        public string serial;
+        public DateTime madeDate;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Compter personPC;
+            personPC.price = 23_750;
+            personPC.serial = "ZCT-20240518A";
+            personPC.madeDate = DateTime.Today;
+            WriteLine($"電腦價格{personPC.price:c0}"+
+                $"\n製造日期{personPC.madeDate:D}"+
+                $"\n序號{personPC.serial}");
+        }
+    }
+}
